d1215676e8ef87fc809b84e24725cd76275a17c5
Avinashk66 <kavinashkumar5125@gmail.com>
2025-12-17 09:25:33 +0530

    Implement divide function in Solution class

---
+++ leetcodes/dividetwo.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+      if (divisor == 0) return INT_MAX;
+        if (dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
+        return dividend / divisor;
+    }
+};
